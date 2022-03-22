f3b46556d47ef7794c44a461aa576bc0f3904a0f
Gabriel Rodrigues <gabrielsr@gmail.com>
2022-03-22 09:46:21 -0300

    Refactoring do_plan interface, for clarifying call

--- crgm/planning.py
+++ crgm/planning.py
@@ -3,21 +3,22 @@ from typing import List
 from crgm.model import Context, Node, Refinement, RefinementType, LeafTask
 
 
-def do_plan(node: Node, context_state: List[Context]):
+def do_plan(initial_node: Node, context_state: List[Context]):
     """  For a given model and context and model, create a plan """
-    if is_leaf_task(node):
-        if is_applicable(node, context_state):
-            return [node]
+    if is_leaf_task(initial_node):
+        if is_applicable(initial_node, context_state):
+            return [initial_node]
         else:
             return None
     else:
-        refinement: Refinement = node.refinement
+        refinement: Refinement = initial_node.refinement
         if refinement.type is RefinementType.OR:
             for child in refinement.children:
                 for child in refinement.children:
                     sub_plan = do_plan(child, context_state)
                     if sub_plan:
                         return sub_plan
+        # TODO implement AND refinement
 
 
 def is_applicable(node, context_state):
--- tests/test_planning.py
+++ tests/test_planning.py
@@ -6,20 +6,20 @@ model = get_model()
 
 
 def test_planning_or_with_valid_context_tasks():
-    plan = do_plan(model.g1, [model.c1])
+    plan = do_plan(initial_node=model.g1, context_state=[model.c1])
     assert plan == [model.p1]
 
 
 def test_planning_or_with_alternative_tasks():
-    plan = do_plan(model.g1, [model.c2])
+    plan = do_plan(initial_node=model.g1, context_state=[model.c2])
     assert plan == [model.p2]
 
 
 def test_planning_or_no_possible_task():
-    plan = do_plan(model.g1, [])
+    plan = do_plan(initial_node=model.g1, context_state=[])
     assert not plan
 
 
 def test_planning_or_many_alternatives():
-    plan = do_plan(model.g1, [model.c1, model.c2])
+    plan = do_plan(initial_node=model.g1, context_state=[model.c1, model.c2])
     assert plan == [model.p1], 'the plan was not the one expected'
